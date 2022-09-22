3e7aa2e87bb13764b65d717e9a8094c09196e8e0
sduzair <sduzairahmed@gmail.com>
2022-09-22 16:03:46 -0400

    all docker images created

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,35 +6,37 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dummyjson.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '^/graphql*': {
-        target: process.env.VITE_GRAPHQL_URL,
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => {
-        //   // path.replace(/^\.\//, '')
-        //   console.log(path);
-        //   return path
-        // }
-      },
-      '^/update/graphql*': {
-        target: process.env.VITE_GRAPHQL_URL,
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => {
-        //   // path.replace(/^\.\//, '')
-        //   console.log(path);
-        //   return path
-        // }
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 4173,
+      proxy: {
+        '/api': {
+          target: 'https://dummyjson.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        '^/graphql*': {
+          target: process.env.VITE_GRAPHQL_URL,
+          changeOrigin: true,
+          secure: false,
+          // rewrite: (path) => {
+          //   // path.replace(/^\.\//, '')
+          //   console.log(path);
+          //   return path
+          // }
+        },
+        '^/update/graphql*': {
+          target: process.env.VITE_GRAPHQL_URL,
+          changeOrigin: true,
+          secure: false,
+          // rewrite: (path) => {
+          //   // path.replace(/^\.\//, '')
+          //   console.log(path);
+          //   return path
+          // }
+        }
       }
     }
-  }
   })
 }
